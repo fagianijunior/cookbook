33641aeb53a64c2005f3c9b44ecbce13ec6ebb4f
Carlos Fagiani Junior <fagianijunior@gmail.com>
2014-11-02 00:39:40 +0000

    bug no js.coffee :/

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,6 @@
 class SearchController < ApplicationController
   def index
-    if params[:q]
-      @users = User.where("first_name like ? or last_name like ?", "%#{params[:q]}%", "%#{params[:q]}%")
-    else
-      @users = User.all.order('created_at DESC')
-    end
+    #if params[:q]
+    @users = User.where("first_name like ? or last_name like ?", "%#{params[:q]}%", "%#{params[:q]}%") || []
   end
 end
